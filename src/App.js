41e0949e14f1499c64a47456693c4e2f6724372f
Paul Patrick <89486089+paul7098@users.noreply.github.com>
2024-12-21 05:04:13 +0000

    Add initial styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <div className="App">
+    <div className="flex w-full min-h-screen flex-col justify-between items-center bg-off-white">
+      <header className="flex w-full h-auto px-4 py-4 justify-between items-center bg-white shadow-sm">
+        <div className="text-black text-4xl font-bold font-['Khand']">LOGO</div>
+        <div className="w-10 h-10 flex-shrink bg-blue-600"></div>
+      </header>
+      <footer className="flex w-full h-auto px-4 py-5 justify-around items-center flex-wrap content-center bg-white shadow-sm">
+        <div className="w-12 h-12 bg-blue-600"></div>
+        <div className="w-12 h-12 bg-blue-600"></div>
+        <div className="w-12 h-12 bg-blue-600"></div>
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'off-white': '#F2F0EF',
+      },
+      fontFamily: {
+        khand: ['Khand', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
